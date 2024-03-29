05c02b2a4fd27d82f68d4396ba2dd8d7a172c5eb
LENOVO <LENOVO@LAPTOP-56DHCMP1>
2024-03-29 00:40:21 -0700

    perbaikan pada if

--- QuickSort.cpp
+++ QuickSort.cpp
@@ -64,9 +64,11 @@ void q_short(int low, int high)
 		}
 		cmp_count++;
 		// search for an element less than or equelto pivot
-		while ((arr[] > pivot) && (j >= low)) // step 7
+		while ((arr[j] > pivot) && (j >= low)) // step 7
 		{
 			j--; // step 8
 			cmp_count++;
 		}
+		cmp_count++;
+		if (1 < j)
 }
