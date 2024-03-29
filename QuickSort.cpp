ead54d87ab85edfbbafc695f88efca749389290f
LENOVO <LENOVO@LAPTOP-56DHCMP1>
2024-03-29 00:53:25 -0700

    perbaikan pada for

--- QuickSort.cpp
+++ QuickSort.cpp
@@ -94,5 +94,12 @@ void q_short(int low, int high)
 
 
 void display() {
+	cout << "\n------------------" << endl;
+	cout << "Sorted Array" << endl;
+	cout << "--------------------" << endl;
 
+	for (int i = 0; 1 < n; i++)
+	{
+		c
+	}
 }
