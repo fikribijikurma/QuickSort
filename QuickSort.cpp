716051339dfcf966661db63f32f1c129847b6947
LENOVO <LENOVO@LAPTOP-56DHCMP1>
2024-03-29 00:22:44 -0700

    masukan int low, int high pada q_short

--- QuickSort.cpp
+++ QuickSort.cpp
@@ -30,4 +30,13 @@ void input() {
 	}
 }
 // swaps the element at index x with the element at index y
-void swap
+void swap(int x, int y)
+{
+	int temp = arr[x];
+	arr[x] = arr[y];
+	arr[y] = temp;
+	mov_count++;
+
+
+}
+
